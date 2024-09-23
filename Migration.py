0a8d7453a646bdd1eccf3097cc4e0c94bfd75840
Mario Junior <122339717+Mario-Juu@users.noreply.github.com>
2024-09-23 18:19:02 -0300

    Initial commit

---
+++ Migration.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import logging
+from sqlalchemy import create_engine
+
+logging.basicConfig(filename='migration.log', level=logging.INFO)
+engine = create_engine('dialect+driver://username:password@host:port/database')
+logging.info("Conectado. Migração iniciada.")
+connection = engine.connect()
+logging.info("Tabela selecionada.")
+query_origem = "SELECT * FROM tabela_migrada"
+
+df = pd.read_sql(query_origem, engine)
+df.to_json('dados_origem.json', index=False)
+
+df.dropna(inplace=True)
+df.drop_duplicates(inplace=True)
+
+engine_destino = create_engine('dialect+driver://username:password@host:port/database')
+
+df.to_sql('usuario', engine_destino, if_exists='append',index=False)
+
+query_contagem = "SELECT COUNT(*) FROM tabela_migrada"
+df_contagem = pd.read_sql(query_contagem, engine_destino)
+print(f'Total de registros no destino: {df_contagem.iloc[0,0]}')
+df = pd.read_sql(query_contagem, engine)
+if df.iloc[0,0] == df_contagem.iloc[0,0]:
+    print("Migração concluida com sucesso")
+else:
+    print("A contagem de registros não bate. Verifique os dados.")
+
